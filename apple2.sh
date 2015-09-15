7c6d077b83e588ca29e6f21110295413ecb5b21b
Robert Fiedor <rfiedor@integralads.com>
2015-09-15 17:27:43 -0400

    asas
    asasa

--- apple2.sh
+++ apple2.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/osascript
-# tell application "Safari" 
-# 	set miniaturized of window 1 to true 
-# end tell
+tell application "Safari"
+  set screen_width to (200 in document 1)
+  set screen_height to (200 in document 1)
+end tell
